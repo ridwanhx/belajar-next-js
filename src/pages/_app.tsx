208c28d2adc0eb95572f2b364ffde8be332e3747
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2026-09-11 23:10:16 +0700

    01. Belajar Next.js: setup next.js, penjelasan tiap bagian kerangka kerja next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
-import '@/styles/globals.css'
+// _app.tsx adalah sebuah route file yang dimana nantinya untuk setiap halaman yang di render, bisa kita atur di sini
+// menjadi titik awal dimana nantinya seluruh url akan mengakses ke file ini terlebih dahulu
+
+// import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,29 @@
+// Next.js bukan hanya bisa digunakan sebagai frontend, namun Next.js ini sifatnya adalah fullstack
+// artinya kita juga bisa membuat backend didalam kerangka kerja Next.js
+// seperti hal-nya yang coba disampaikan pada contoh berikut, dimana kita pada folder ini bisa mendefinisikan API
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+// inisialisasi tipe data
 type Data = {
-  name: string
+  // berisi nama properti dan tipe data nya
+  name: string,
+  age: number,
+  hobbies: object
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // inisialisasi response yang akan mengembalikan status code 200 dengan data json seperti yang terlihat di bawah
+  res.status(200).json({
+    name: 'Muhamad Ridwan',
+    age: 23,
+    hobbies: {
+      productive: "coding",
+      chill: "gaming"
+    }
+  })
 }
