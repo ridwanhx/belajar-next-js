e8da7589a9d779c406a427e07b9c0913ebe22e1e
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2026-09-23 00:05:04 +0700

    feat(component): redesign navbar, styling with scss and tailwind

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,13 +1,33 @@
-import navbarStyles from "./Navbar.module.scss"
+import styles from "./Navbar.module.scss"
+import {Icon} from "@iconify/react"
 export default function Navbar() {
     return (
-        <nav className={navbarStyles.navbar}>
-            <a href="#" className={navbarStyles.header}>Navbar</a>
-            <ul className={navbarStyles.items}>
-                <li><a href="#" className={navbarStyles.item}>Home</a></li>
-                <li><a href="#" className={navbarStyles.item}>Service</a></li>
-                <li><a href="#" className={navbarStyles.item}>About</a></li>
-                <li><a href="#" className={navbarStyles.item}>Contact</a></li>
+        <nav className={styles.navigations__navbar}>
+            <div className="flex items-center gap-8">
+                <a href="#" className={styles.navigations__header}>MYMDB</a>
+                <ul className={styles.navigations__items}>
+                    <li><a href="#" className={styles.navigations__item}>Home</a></li>
+                    <li><a href="#" className={styles.navigations__item}>Service</a></li>
+                    <li><a href="#" className={styles.navigations__item}>About</a></li>
+                    <li><a href="#" className={styles.navigations__item}>Contact</a></li>
+                </ul>
+            </div>
+            <ul className="flex items-center gap-8 flex-row-reverse font-semibold">
+                    <li>
+                        <Icon icon={"bi:search"} className="text-2xl cursor-pointer" />
+                    </li>
+                    <li>
+                        <a href="#" className="capitalize tracking-tight">join <span className="uppercase">mymdb</span></a>
+                    </li>
+                    <li>
+                        <a href="#" className="capitalize tracking-tight">Login</a>
+                    </li>
+                    <li>
+                        <a href="#" className="uppercase tracking-tight border p-1.5 rounded-sm text-sm">en</a>
+                </li>
+                <li>
+                        <Icon icon={"at-icons:plus"} className="text-2xl cursor-pointer" />
+                    </li>
             </ul>
         </nav>
     );
