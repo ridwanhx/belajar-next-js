7a0757dfa4587a7e425124c0182829e5845fd5c0
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2026-09-23 00:33:49 +0700

    feat(component): adding subnavbar component

--- src/components/layouts/SubNavbar/index.tsx
+++ src/components/layouts/SubNavbar/index.tsx
@@ -1,18 +1,24 @@
+import {Icon} from "@iconify/react"
+
 export default function SubNavbar() {
     return (
-        <nav>
-            <ul>
-                <li>
+        <nav className="w-full p-4 shadow-sm">
+            <ul className="flex gap-8 justify-center text-sm">
+                <li className="flex items-center gap-1.2 hover:text-slate-400">
                     <a href="">Overview</a>
+                    <Icon icon={"basil:caret-down-outline"} className="text-lg"/>
                 </li>
-                <li>
+                <li className="flex items-center gap-1.2 hover:text-slate-400">
                     <a href="">Media</a>
+                    <Icon icon={"basil:caret-down-outline"} className="text-lg"/>
                 </li>
-                <li>
+                <li className="flex items-center gap-1.2 hover:text-slate-400">
                     <a href="">Fandom</a>
+                    <Icon icon={"basil:caret-down-outline"} className="text-lg"/>
                 </li>
-                <li>
+                <li className="flex items-center gap-1.2 hover:text-slate-400">
                     <a href="">Share</a>
+                    <Icon icon={"basil:caret-down-outline"} className="text-lg"/>
                 </li>
             </ul>
         </nav>
